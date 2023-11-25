4d37df91fad04b2c1acf1857629d78c25968fa0c
JeldrikGaal <jeldrikgaal@gmail.com>
2023-11-25 11:03:07 +0100

    Added thought pop up to player

--- Assets/_Scripts/Player/PlayerThoughts.cs
+++ Assets/_Scripts/Player/PlayerThoughts.cs
@@ -6,28 +6,40 @@ using UnityEngine;
 public class PlayerThoughts : MonoBehaviour
 {
     [SerializeField] private WorldText _thoughtText;
+    [SerializeField] private GameObject _thoughtBubbleObject;
 
     [SerializeField] private List<string> _pickedUpThought;
     [SerializeField] private const string _pickedUpThoughtText = "I picked something up";
+    [SerializeField] private float _thoughtLingerTime;
     
     private void OnEnable()
     {
-        throw new NotImplementedException();
+        PickupInteractable.PickupCollected += ShowPickedUpThought;
     }
 
     private void OnDisable()
     {
-        throw new NotImplementedException();
+        PickupInteractable.PickupCollected -= ShowPickedUpThought;
     }
+
+    private void ShowPickedUpThought(PickupInteractable obj)
+    {
+        SetThoughtText(_pickedUpThoughtText);
+    }
+
 
     public void ClearThoughtText()
     {
-        _thoughtText.SetTextAndTime("", 0f);
+        _thoughtText.ClearText();
+        _thoughtBubbleObject.SetActive(false);
     }
     
     public void SetThoughtText(string text)
     {
-        _thoughtText.SetTextAndTime(text, text.Length * 0.05f);
-        //  Invoke();
+        _thoughtBubbleObject.SetActive(true);
+        float writeTime = text.Length * 0.05f;
+        _thoughtText.SetTextAndTime(text, writeTime);
+        _thoughtText.ShowTextByLetter();
+        Invoke(nameof(ClearThoughtText), _thoughtLingerTime +  writeTime ); 
     }
 }
