a04a4aec5542a3c1cb9919bfac6e51385df40030
Loai Qubti <loai17@meet.mit.edu>
2023-11-25 14:21:04 +0100

    Adding TheEvent Scene + Camera Limit Locker

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -18,6 +18,18 @@ public class CameraController : MonoBehaviour
 
     private float timer = 0;
 
+    [Space(20)] public bool xLimit1;
+    public float x1Limit1 = 0;
+    public float x2Limit1 = 0;
+    
+    [Space(20)] public bool xLimit2;
+    public float x1Limit2 = 0;
+    public float x2Limit2 = 0;
+
+    [Space(20)] public bool xLimit3;
+    public float x1Limit3 = 0;
+    public float x2Limit3 = 0;
+
     private void Start()
     {
         lastX = target.position.x;
@@ -25,30 +37,43 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= triggerDelay)
+        if (xLimit1 || xLimit2 || xLimit3)
         {
-            timer = 0;
-            if (target.position.x > lastX)
+            if (xLimit1 && transform.position.x <= x1Limit1) { transform.position = new Vector3(x1Limit1,transform.position.y,transform.position.z); }
+            else if (xLimit1 && transform.position.x >= x2Limit1) { transform.position = new Vector3(x2Limit1, transform.position.y,transform.position.z); }
+            else if(xLimit2 && transform.position.x <= x1Limit2){transform.position = new Vector3(x1Limit2, transform.position.y,transform.position.z);}
+            else if(xLimit2 && transform.position.x >= x2Limit2){transform.position = new Vector3(x2Limit2, transform.position.y,transform.position.z);} 
+            else if(xLimit3 && transform.position.x <= x1Limit3){transform.position = new Vector3(x1Limit3, transform.position.y,transform.position.z);} 
+            else if(xLimit3 && transform.position.x >= x2Limit3){transform.position = new Vector3(x2Limit3, transform.position.y,transform.position.z);} 
+        }
+        else
+        {
+            timer += Time.deltaTime;
+            if (timer >= triggerDelay)
             {
-                // Player walking to the right
-                offset.x = targetXOffset;
+                timer = 0;
+                if (target.position.x > lastX)
+                {
+                    // Player walking to the right
+                    offset.x = targetXOffset;
 
-            }
-            else if (target.position.x < lastX)
-            {
-                // Player walking to the left
-                offset.x = -targetXOffset;
-            }
-            else
-            {
-                // Player stopped walking.
-                offset.x = 0;
-            }
+                }
+                else if (target.position.x < lastX)
+                {
+                    // Player walking to the left
+                    offset.x = -targetXOffset;
+                }
+                else
+                {
+                    // Player stopped walking.
+                    offset.x = 0;
+                }
 
-            lastX = target.position.x;
+                lastX = target.position.x;
+            }
         }
     }
+
     private void LateUpdate()
     {
         transform.position = Vector3.SmoothDamp(
