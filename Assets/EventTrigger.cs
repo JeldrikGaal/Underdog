59522af9de1f6f963ed2a4d973715a96653000ea
Loai Qubti <loai17@meet.mit.edu>
2023-11-25 21:48:43 +0100

    Added LevelManager and EventTrigger

--- Assets/EventTrigger.cs
+++ Assets/EventTrigger.cs
@@ -1,25 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EventTrigger : MonoBehaviour
 {
     public GameObject GO;
-    public AudioClip soundFX;
     public bool camShake;
     public AnimationCurve shakeCurve;
     
-    private 
+    [Space(20)]
+    public float delay = 0.5f;
+    public float destroyAfter = 2f;
     
-    // Start is called before the first frame update
+    private GameObject mainCam;
+    private bool eventTriggered = false;
+
     void Start()
     {
-        
+        mainCam = GameObject.FindWithTag("MainCamera");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Player" && !eventTriggered)
+        {
+            eventTriggered = true;
+            StartCoroutine(TriggerEvent(delay));
+        }
     }
+
+    IEnumerator TriggerEvent(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        
+        GO.SetActive(true);
+        StartCoroutine(mainCam.GetComponent<CamShake>().CustomShake(shakeCurve));
+        
+        Destroy(GO, destroyAfter);
+        Destroy(this.gameObject, destroyAfter);
+    }
+
 }
