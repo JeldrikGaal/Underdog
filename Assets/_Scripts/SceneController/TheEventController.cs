8ac23bfdbd27705fb5370b3f4ea456282698fa62
JeldrikGaal <jeldrikgaal@gmail.com>
2023-11-26 13:13:25 +0100

    Start work on the even scene

---
+++ Assets/_Scripts/SceneController/TheEventController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheEventController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _lookingGuard1;
+    [SerializeField] private GameObject _lookingGuard2;
+
+
+    private void Start()
+    {
+        ActivateSoldiersWithDelay();
+    }
+
+    private void ActivateSoldiersWithDelay()
+    {
+        Invoke(nameof(ActivateSolider1), 0.5f);
+        Invoke(nameof(ActivateSolider2), 1f);
+    }
+
+    private void ActivateSolider1()
+    {
+        _lookingGuard1.SetActive(true);
+    }
+    
+    private void ActivateSolider2()
+    {
+        _lookingGuard2.SetActive(true);
+    }
+    
+    
+    private void ClimbAnimation()
+    {
+        
+    }
+    
+}
